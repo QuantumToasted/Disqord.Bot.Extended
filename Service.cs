60b6c7fa572418b649acd327a316bb9b4a9c053b
Daniel Jarski <danieljarski@gmail.com>
2020-05-17 01:31:29 -0500

    Changed access level for the bot in Service.
    (private protected only applies to the same assembly, which is invalid for this use case)

--- Service.cs
+++ Service.cs
@@ -1,19 +1,33 @@
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Disqord.Logging;
 
 namespace Disqord.Bot.Extended
 {
-    public abstract class Service<TBot>
+    /// <summary>
+    /// A service which is automatically injected into your bot's service provider.
+    /// </summary>
+    /// <typeparam name="TBot">
+    /// The type of bot to pass into the service via Dependency Injection.
+    /// <para>Should match your custom inherited <see cref="ExtendedDiscordBot"/> type.</para> 
+    /// </typeparam>
     public abstract class Service<TBot> : IInitializable
         where TBot : ExtendedDiscordBot
     {
-        private protected readonly TBot _bot;
+        /// <summary>
+        /// The bot which was passed into the service.
+        /// </summary>
+        protected readonly TBot _bot;
 
         protected Service(TBot bot)
         {
             _bot = bot;
         }
 
+        /// <summary>
+        /// Overrides the default initialization behavior. This method is called automatically when the bot runs.
+        /// <para>For best results, call <c>base.InitializeAsync</c> when overriding this method.</para>
+        /// </summary>
+        /// <returns></returns>
         public virtual ValueTask InitializeAsync()
         {
             _bot.Log(GetType().Name, LogMessageSeverity.Information, "Service initialized.");
